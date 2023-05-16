ecd85a364a8306dd0a4b6026d4e9a3753023bfa4
rony joseph <ronyjoseph1998@gmail.com>
2023-05-16 22:11:06 +0530

    chages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
+import { EducationComponent } from './education/education.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { ResponsiveDirective } from './directives/responsive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    PersonalInformationComponent,
+    EducationComponent,
+    ContactComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    WorkExperienceComponent,
+    ResponsiveDirective
   ],
   imports: [
     BrowserModule,
